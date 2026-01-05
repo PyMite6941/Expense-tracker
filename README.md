996e7b61a6391ea1f5cab6866d478162ad3b26a0
-XO-OX- <greshamd27@gmail.com>
2026-01-05 21:59:55 +0900

    version 1
    Add expenses and view them whenever.

---
+++ README.md
@@ -0,0 +1,16 @@
+## ABOUT ##
+This is a program that helps keep track of inputted expenses.
+You can filter and view expenses as well using the main.py file.
+
+## SETUP ##
+First run the virtual environment (venv) setup:
+python -m venv {your_venv_directory_name}
+
+Then, if Windows is being used run:
+{your_venv_directory_name}\Scripts\python.exe
+
+If you are on Mac or Linux run:
+source {your_venv_directory_name}\bin\activate
+
+Finally you are ready to run the file using the following command:
+python main.py
---
+++ main.py
@@ -0,0 +1,91 @@
+from datetime import datetime
+import json
+
+class ExpenseTracker():
+    def __init__(self,filename='data.txt'):
+        self.filename = filename
+
+    def open_file(self) -> list:
+        expense_data = []
+        running = True
+        try:
+            with open(self.filename, 'r') as file:
+                data = json.load(file)
+            return data
+        except FileNotFoundError:
+            print("File doesn't exist, creating ...")
+            json.dump(self.filename, 'w')
+            return []
+    
+    def write_file(self,list:list):
+        not_worked = True
+        while not_worked:
+            try:
+                with open(self.filename, 'w') as file:
+                    json.dump(list,file)
+            except FileNotFoundError:
+                print("File doesn't exist")
+
+    def view_total_expenses(self) -> list:
+        data = self.open_file()
+        count = 1
+        for item in data:
+            print(f"\nExpense {count}\n\nPrice: {item['price']}\nPurchased: {item['purchased']}\nDate spent: {item['date']}")
+            count += 1
+
+    def view_filtered_expenses(self) -> list:
+        filter_choice = int(input("--- Filter Menu ---\n\n1. By Price\n2. By purchased item\n3. Date\nYour choice:\n> "))
+        if filter_choice == 1:
+            filter_min_value = float(input("Enter the minimum value:\n> "))
+            filter_max_value = float(input("Enter the maximum value:\n> "))
+        elif filter_choice == 2:
+            filter_item = str(input("Enter the item purchased:\n> "))
+        elif filter_choice == 3:
+            filter_min_date = input("Enter the start range (yyyy-mm-dd):\n> ")
+            filter_max_date = input("Enter the end range (yyyy-mm-dd):\n> ")
+        else:
+            print("Choose from the above menu")
+            return []
+        data = self.open_file()
+        results = []
+        if filter_choice == 1:
+            for item in data:
+                if filter_min_value <= item['price'] <= filter_max_value:
+                    results.append(item)
+        elif filter_choice == 2:
+            for item in data:
+                if filter_item == item['purchased']:
+                    results.append(item)
+        elif filter_choice == 3:
+            for item in data:
+                if filter_min_date <= item['date'] <= filter_max_date:
+                    results.append(item)
+        return results
+
+    def add_expenses(self,price:float,purchased:str,date=f'{datetime.now().strftime("%Y-%m-%d")}') -> bool:
+        expense = {
+            'price': price,
+            'purchased': purchased,
+            'date': date,
+        }
+        data = self.open_file()
+        data.append(expense)
+        self.write_file(data)
+        return True
+
+tracker = ExpenseTracker()
+running = True
+while running:
+    choice = int(input("--- Menu ---\n\n1. View total expenses\n2. Filter total expenses\n3. Add expenses\n0. Exit\nYour choice:\n> "))
+    if choice == 1:
+        expenses = tracker.view_total_expenses()
+        print(expenses)
+    elif choice == 2:
+        filtered_expenses = tracker.view_filtered_expenses()
+        print(filtered_expenses)
+    elif choice == 3:
+        price = float(input("How much was spent?\n> "))
+        purchased = input("What was purchased?\n> ")
+        added_expenses = tracker.add_expenses(price,purchased)
+    elif choice == 0:
+        running = False
