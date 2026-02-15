af3135f06440d4ab914da8abbacc91f41e5231f5
-XO-OX- <greshamd27@gmail.com>
2026-02-15 13:58:49 +0700

    Started Web UI updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,25 @@
 import streamlit as st
 
+from core_app import ExpenseTracker
+
+# If class obj not in session state then import
+if tracker not in st.session_state:
+    st.session_state.tracker = ExpenseTracker()
+# If expenses not in session state then import
 if 'expenses' not in st.session_state:
-    st.session_state.expenses = []
+    results = st.session_state.tracker.view_total_expenses()
+    st.session_state.expenses = results['data'] if results['success'] else []
+# If income not in session state then import
+if 'income' not in st.session_state:
+    results = st.session_state.tracker.view_total_income()
+    st.session_state.income = results['data'] if results['success'] else []
+# If budgets not in session state then import
+if 'budget' not in st.session_state:
+    results = st.session_state.tracker.view_total_budgets()
+    st.session_state.budget = results['data'] if results['success'] else []
+
 st.title('Web-based Expense and Income Tracking')
+
 
 st.button('View Total Expenses')
 st.button('View Total Income')
